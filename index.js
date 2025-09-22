9aee7dfc6c0a86689d069563d6c9c47e6d95a1bf
j-ami-l <mdhasanjamil0001@gmail.com>
2025-09-22 19:39:02 +0600

    set up server for apnrghor website

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const app = express()
+app.use(express.json())
+app.use(cors({
+  origin: [
+    "http://localhost:5173"
+
+  ],
+  credentials: true
+}))
+
+const port = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+  res.send("sharemeal server is running")
+})
+
+
+
+app.listen(port, () => {
+  console.log("server is running");
+
+})
